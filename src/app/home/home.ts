cf63bb723beb5f9156600eed69856f82be7a52bd
hacha-99 <hamid.charif99@gmail.com>
2025-09-30 17:48:06 +0200

    more data-passing

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Greeter } from '../components/greeter/greeter';
 
 @Component({
@@ -8,5 +8,5 @@ import { Greeter } from '../components/greeter/greeter';
   styleUrl: './home.scss'
 })
 export class Home {
-
+    messageHome = signal("Greetings from a variable from home component!");
 }

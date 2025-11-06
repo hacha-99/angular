3d0c4473d492ad1b5df60dd9be91938549c539e0
hacha-99 <hamid.charif99@gmail.com>
2025-11-06 15:36:30 +0100

    first http call and rendering of list using @for

--- src/app/todos/todos.ts
+++ src/app/todos/todos.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { TodosService } from '../services/todos-service';
+import { Todo } from '../model/todo.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-todos',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
   templateUrl: './todos.html',
   styleUrl: './todos.scss'
 })
-export class Todos {
+export class Todos implements OnInit {
+    todoService = inject(TodosService);
+    todoItems = signal<Array<Todo>>([]);
 
+    ngOnInit(): void {
+        this.todoService
+        .getTodosFromApi()
+        .pipe(
+            catchError((err) => {
+                console.log(err);
+                throw err;
+            })
+        )
+        .subscribe((todos) => {
+            this.todoItems.set(todos);
+        });
+    }
 }
